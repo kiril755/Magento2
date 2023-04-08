8776091057b1f2d636f2f377c4ad3ad85834f43c
kiril755 <kiril777kiril@gmail.com>
2023-04-09 01:09:57 +0300

    fill the name and email if user loggined (task 3)

--- app/code/Task/ProductCommentModule/Block/Comment.php
+++ app/code/Task/ProductCommentModule/Block/Comment.php
@@ -4,25 +4,43 @@ namespace Task\ProductCommentModule\Block;
 
 use Magento\Framework\View\Element\Template;
 use Task\ProductCommentModule\Model\ResourceModel\Item\CollectionFactory;
+use Magento\Customer\Model\Session;
 
 
 class Comment extends Template
 {
     private $collectionFactory;
+    protected $customerSession;
 
     public function __construct(
         Template\Context $context,
         CollectionFactory $collectionFactory,
+//        Magento\Customer\Model\Session $customerSession,
         array $data = []
     )
     {
+
         $this->collectionFactory = $collectionFactory;
         parent::__construct($context, $data);
+//        $this->customerSession = $customerSession;
     }
 
+    public function getUserData () {
+        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        $customerSession = $objectManager->get('Magento\Customer\Model\Session');
+
+        $userData = [];
+
+        if($customerSession->isLoggedIn()) {
+            $name = $customerSession->getCustomerData()->getFirstname();
+            $email = $customerSession->getCustomerData()->getEmail();
+            return $userData = ['email' => $email, 'name' => $name];
+        }
+    }
 
     /** @return \Task\ProductCommentModule\Model\Item[]  */
     public function getItems () {
+        
         return $this->collectionFactory->create()->addFieldToFilter('status', 'success');
 
     }
