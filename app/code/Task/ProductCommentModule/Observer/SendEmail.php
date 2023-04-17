d9b41ee2c3d5f4d12f439371bab58d8d67b8734d
kiril755 <kiril777kiril@gmail.com>
2023-04-17 23:36:46 +0300

    fix observer
    fix observer, that send mail (add 5 last pending comments)

--- app/code/Task/ProductCommentModule/Observer/SendEmail.php
+++ app/code/Task/ProductCommentModule/Observer/SendEmail.php
@@ -2,21 +2,45 @@
 namespace Task\ProductCommentModule\Observer;
 
 use Magento\Framework\Event\ObserverInterface;
+use Task\ProductCommentModule\Model\ResourceModel\Item\CollectionFactory;
 
 class SendEmail implements ObserverInterface
 {
+    private $commentCollectionFactory;
+
+    public function __construct(CollectionFactory $commentCollectionFactory) {
+        $this->commentCollectionFactory = $commentCollectionFactory;
+    }
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
         $request = $observer->getEvent()->getObject();
 
+        // get data of last created comment
         $nickname = $request->getNickname();
         $email = $request->getEmail();
-        $comment = $request->getText();
+        $commentValue = $request->getText();
 
-        $message = "Please, don't forget to confirm or reject this comment:\n\n"
+        // get data of last 4 comment with pending status
+
+        $commentCollection = $this->commentCollectionFactory->create();
+        $commentCollection
+            ->addFieldToFilter('status', 'pending')
+            ->setOrder('create_time', 'desc')
+            ->setPageSize(5)
+            ->load();
+        $lastFiveComment = '';
+
+        foreach ($commentCollection as $comment) {
+            $lastFiveComment .= "nickname: " . $comment->getNickname() . "\n"
+                . "email: " . $comment->getEmail() . "\n"
+                . "Comment: " . $comment->getText() . "\n\n";
+        }
+
+        $message = "Please, don't forget to confirm or reject this new comment:\n\n"
             . "nickname: " . $nickname . "\n"
             . "email: " . $email . "\n"
-            . "Comment: " . $comment . "\n";
+            . "Comment: " . $commentValue . "\n\n\n"
+            . "The last 5 unapproved or unrejected comments:\n\n" . $lastFiveComment;
         $headers = 'From: ' . $email;
 
         mail("owner@example.com", "New Comment", $message, $headers);
