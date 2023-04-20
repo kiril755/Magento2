badf8402176af0a37f1cf51e6165badaeb02cbe9
kiril755 <kiril777kiril@gmail.com>
2023-04-20 16:41:32 +0300

    add some data to Selected Products

--- app/code/Comment/CommentProductModule/Block/ProductListBlock.php
+++ app/code/Comment/CommentProductModule/Block/ProductListBlock.php
@@ -12,16 +12,19 @@ class ProductListBlock extends Template
     protected $productRepository;
     protected $selectedProductIds;
     protected $searchCriteriaBuilder;
+    protected $_imageHelper;
 
     public function __construct(
         Context $context,
         ProductRepositoryInterface $productRepository,
         SearchCriteriaBuilder $searchCriteriaBuilder,
+        \Magento\Catalog\Helper\Image $imageHelper,
         array $data = []
     ) {
         parent::__construct($context, $data);
         $this->productRepository = $productRepository;
         $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->_imageHelper = $imageHelper;
         $this->selectedProductIds = explode(',', $this->_scopeConfig->getValue('comment/general/product_id'));
     }
 
@@ -29,6 +32,11 @@ class ProductListBlock extends Template
     {
         $searchCriteria = $this->searchCriteriaBuilder->addFilter('entity_id', $this->selectedProductIds, 'in')->create();
         $result = $this->productRepository->getList($searchCriteria);
-        return $result;
+        $productCollection = $result->getItems();
+        foreach ($productCollection as &$product) {
+            $image = $this->_imageHelper->init($product, 'product_base_image')->getUrl();
+            $product->setImage($image);
+        }
+        return $productCollection;
     }
 }
