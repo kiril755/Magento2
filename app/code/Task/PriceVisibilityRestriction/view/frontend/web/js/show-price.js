2cf60e0c707c736a6978d6b3de70b212deb24a5e
kiril755 <kiril777kiril@gmail.com>
2023-05-02 13:15:03 +0300

    refactore code

--- app/code/Task/PriceVisibilityRestriction/view/frontend/web/js/show-price.js
+++ app/code/Task/PriceVisibilityRestriction/view/frontend/web/js/show-price.js
@@ -5,6 +5,28 @@ define(['jquery','domReady!'], function ($) {
             const url = '/price_request/ajax/showprice';
             const priceDiv = $('<div>').addClass('price');
             $(element).attr('disabled', true);
+            const addToCartTemplate = '<div class="box-tocart"">\n' +
+                '            <div class="fieldset">\n' +
+                '                <div class="field qty">\n' +
+                '                    <label class="label" for="qty"><span>Qty</span></label>\n' +
+                '                    <div class="control">\n' +
+                '                        <input type="number" name="qty" id="qty" min="0" value="1" title="Qty" class="input-text qty" data-validate="{&quot;required-number&quot;:true,&quot;validate-item-quantity&quot;:{&quot;maxAllowed&quot;:10000}}">\n' +
+                '                    </div>\n' +
+                '                </div>\n' +
+                '                <div class="actions">\n' +
+                '                    <button type="submit" title="Add to Cart" class="action primary tocart" id="product-addtocart-button">\n' +
+                '                        <span>Add to Cart</span>\n' +
+                '                    </button>\n' +
+                '\n' +
+                '                    <div id="instant-purchase" data-bind="scope:\'instant-purchase\'">\n' +
+                '                        <!-- ko template: getTemplate() -->\n' +
+                '                        <!-- ko if: showButton() --><!-- /ko -->\n' +
+                '                        <!-- /ko -->\n' +
+                '                    </div>\n' +
+                '\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '        </div>';
             $.ajax({
                 url: url,
                 type: 'POST',
@@ -17,11 +39,9 @@ define(['jquery','domReady!'], function ($) {
                     product_id: config.productId
                 },
                 success: function (response) {
-                    console.log(response)
                     if (response.price) {
                         $('.product-info-stock-sku').before(priceDiv.text(config.currency + parseFloat(response.price).toFixed(2)));
-                        $('.product-options-bottom').show();
-                        $('.box-tocart').show();
+                        $(element).after(addToCartTemplate);
                         $(element).hide();
                     } else {
                         console.log(response.error);
